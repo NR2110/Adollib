d42c3939118997441f6109af71dd0971a86683a5
NR2110 <waaaaanya2110@icloud.com>
2020-11-04 02:19:15 +0900

    明日はk-dopの更新部分から

--- Adollib/Adollib/Scripts/Physics/ALP_shape.cpp
+++ Adollib/Adollib/Scripts/Physics/ALP_shape.cpp
@@ -36,39 +36,39 @@ void ALP_Shape::update_dop14_as_sphere() {
 }
 
 void ALP_Shape::update_dop14_as_box() {
-	dop14.pos = ALP_coll + local_position;
+	//dop14.pos = ALP_coll + local_position;
 
-	//各頂点のローカル座標
-	Vector3 half[4]{
-	{+half_size.x, -half_size.y, -half_size.z},
-	{+half_size.x, -half_size.y, +half_size.z},
-	{+half_size.x, +half_size.y, -half_size.z},
-	{+half_size.x, +half_size.y, +half_size.z}
-	};
+	////各頂点のローカル座標
+	//Vector3 half[4]{
+	//{+half_size.x, -half_size.y, -half_size.z},
+	//{+half_size.x, -half_size.y, +half_size.z},
+	//{+half_size.x, +half_size.y, -half_size.z},
+	//{+half_size.x, +half_size.y, +half_size.z}
+	//};
 
-	for (int i = 0; i < 4; i++) {
-		half[i] = half[i] * world_scale;
-	}
+	//for (int i = 0; i < 4; i++) {
+	//	half[i] = half[i] * world_scale;
+	//}
 
-	Quaternion WO = gameobject->get_world_orientate();
-	for (int i = 0; i < 4; i++) {
-		half[i] = vector3_quatrotate(half[i], WO);
-	}
+	//Quaternion WO = gameobject->get_world_orientate();
+	//for (int i = 0; i < 4; i++) {
+	//	half[i] = vector3_quatrotate(half[i], WO);
+	//}
 
-	//DOPの更新
-	float max_len = 0;
-	for (int i = 0; i < DOP_size; i++) {
-		max_len = 0;
-		for (int o = 0; o < 4; o++) {
-			float dis = fabsf(vector3_dot(DOP_14_axis[i], half[o]));
-			if (max_len < dis) {
-				dop14.max[i] = +dis * 1.00000001f;//確実にするためちょっと大きめにとる
-				dop14.min[i] = -dis * 1.00000001f;//確実にするためちょっと大きめにとる
-				max_len = dis;
-			}
+	////DOPの更新
+	//float max_len = 0;
+	//for (int i = 0; i < DOP_size; i++) {
+	//	max_len = 0;
+	//	for (int o = 0; o < 4; o++) {
+	//		float dis = fabsf(vector3_dot(DOP_14_axis[i], half[o]));
+	//		if (max_len < dis) {
+	//			dop14.max[i] = +dis * 1.00000001f;//確実にするためちょっと大きめにとる
+	//			dop14.min[i] = -dis * 1.00000001f;//確実にするためちょっと大きめにとる
+	//			max_len = dis;
+	//		}
 
-		}
-	}
+	//	}
+	//}
 }
 
 void ALP_Shape::update_dop14_as_plane() {
