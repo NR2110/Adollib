bdbe0570ca5da82dfef4921b8d5090c61af75131
NR2110 <waaaaanya2110@icloud.com>
2021-02-02 22:15:50 +0900

    #define ALCover #define ALEasing の追加

--- Adollib/Adollib/Scripts/Math/math_e.h
+++ Adollib/Adollib/Scripts/Math/math_e.h
@@ -15,6 +15,8 @@ namespace Adollib {
 #define ALmax(a,b) (a>b?a:b)
 #define ALmin(a,b) (a<b?a:b)
 #define ALClamp(a,min_,max_) (ALmin(ALmax(a,min_),max_))
+#define ALCover(a,b,c) (((a - b) * (c - b)) < 0)
+#define ALEasing(start,goal,speed,delta_time) goal + (start - goal) * pow(1 - speed, delta_time * 60)
 
 	typedef unsigned char   u_char;
 	typedef unsigned short  u_short;
